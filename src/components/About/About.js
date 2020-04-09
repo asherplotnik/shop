139320e4f4902e336bed449d0a20c9a19a5a3b8d
Asher <asherplotnik@gmail.com>
2020-04-10 02:53:02 +0700

    before branching

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,16 +1,6 @@
 import React from "react";
 import axios from "axios";
 const about = () => {
-  const doThis = () => {
-    let sql = {
-      sql:
-        "UPDATE collections set `desc` = 'PEARLS COLLECTION' WHERE name = 'PEARLS'",
-    };
-    axios.post("http://localhost:9000/API/update", sql).then((response) => {
-      console.log(response);
-    });
-  };
-
   return (
     <div>
       <h1>About</h1>
